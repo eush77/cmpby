2c0352d3a443c9016fe4474de0b314fe28c1eee1
Eugene Sharygin <eush77@gmail.com>
2014-12-13 01:44:17 +0300

    Make keyfn optional

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ var declared = require('declared');
 /**
  * Create comparator by key.
  *
- * @arg {obj -> key} keyfn
+ * @arg {obj -> key} [keyfn] - Key function, identity by default.
  * @arg {Object} [options]
  * @property {(key1, key2) -> boolean} [less] - Key comparison function.
  * @property {boolean} [asc=true] - Ascending or descending.
  * @return {(obj1, obj2) -> number}
  */
 module.exports = function (keyfn, options) {
+  if (typeof keyfn != 'function') {
+    options = keyfn;
+    keyfn = function (x) { return x; };
+  }
+
   options = options || {};
   options.less = options.less || function (a, b) { return a < b; };
   options.asc = declared(options.asc, true);
@@ -25,7 +30,7 @@ module.exports = function (keyfn, options) {
       return 0;
     }
 
-    var keyLess = options.less(keyfn(a), keyfn(b));
+    var keyLess = options.less(ka, kb);
     return (options.asc ? keyLess : !keyLess) ? -1 : 1;
   };
 };
